32210e0790f52a7435ee2a557dd01c0ef2836408
Polyalves2 <alvespoly02@gmail.com>
2025-07-20 23:47:52 -0300

    atividade3

---
+++ atividade3.php
@@ -0,0 +1,10 @@
+<?php
+// atividade 3 //
+
+$numero = 5; 
+echo "<h3>Tabuada do $numero</h3>";
+for ($i = 1; $i <= 10; $i++) {
+    $resultado = $numero * $i;
+    echo "$numero x $i = $resultado<br>";
+}
+?>
